4fb220a87cde6cc7dfa0fd9294d141efd436e2aa
Defanda Yeremia <144663115+doctordoom101@users.noreply.github.com>
2026-05-29 21:17:32 +0700

    UPDATE: image segmentation

--- api/main.py
+++ api/main.py
@@ -19,9 +19,13 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 # Daftarkan folder uploads agar gambarnya bisa diakses lewat URL browser
 app.mount("/static", StaticFiles(directory=UPLOAD_DIR), name="static")
 
-# Load model .keras utama kamu (Muat 1 kali di awal startup biar kencang)
-MODEL_PATH = "../model/mobilenetv2/best_mobilenetv2_model.keras" 
-model = tf.keras.models.load_model(MODEL_PATH)
+# Load model .keras utama kamu (Muat di awal startup biar kencang)
+MODEL_CLS_PATH = "../model/mobilenetv2/best_mobilenetv2_model.keras" 
+MODEL_SEG_PATH = "../model/unet/best_unet.keras"
+
+print("Loading Models...")
+model_cls = tf.keras.models.load_model(MODEL_CLS_PATH)
+model_seg = tf.keras.models.load_model(MODEL_SEG_PATH, compile=False)
 
 # Fungsi pembantu untuk memanggil session database (Dependency Injection)
 def get_db():
@@ -47,17 +51,18 @@ async def predict_xray(file: UploadFile = File(...), db: Session = Depends(get_d
     with open(orig_path, "wb") as buffer:
         buffer.write(await file.read())
 
-    # 3. Jalankan Preprocessing & Prediksi Model
-    img_tensor = utils.preprocess_image(orig_path)
-    predictions = model.predict(img_tensor)
-    confidence = float(predictions[0][0])
+    # 3. Jalankan Preprocessing & Prediksi Klasifikasi (MobileNetV2)
+    img_cls_tensor = utils.preprocess_classification(orig_path)
+    cls_predictions = model_cls.predict(img_cls_tensor)
+    confidence = float(cls_predictions[0][0])
     
     label = "Tuberculosis" if confidence > 0.5 else "Normal"
     # Siasat confidence score agar selalu mencerminkan kelas terpilih
     display_confidence = confidence if label == "Tuberculosis" else (1.0 - confidence)
 
-    # 4. Jalankan Grad-CAM Masking
-    utils.generate_and_save_gradcam(model, img_tensor, orig_path, vis_path)
+    # 4. Jalankan Lung Segmentation (U-Net) & Save Overlay
+    img_seg_tensor = utils.preprocess_segmentation(orig_path)
+    utils.save_segmentation_overlay(model_seg, img_seg_tensor, orig_path, vis_path)
 
     # 5. Simpan catatan ke SQLite Database
     db_record = models_db.Prediction(
@@ -76,7 +81,7 @@ async def predict_xray(file: UploadFile = File(...), db: Session = Depends(get_d
         "prediction": label,
         "confidence": f"{display_confidence * 100:.2f}%",
         "original_image_url": f"/static/{orig_filename}",
-        "masking_image_url": f"/static/{vis_filename}",
+        "segmentation_image_url": f"/static/{vis_filename}",
         "created_at": db_record.created_at
     }
 
